4eb5e74a659901f350e33e699b579667922920d8
Davi Garcia de Souza <davigs@hotmail.com>
2020-11-19 18:22:44 -0300

    exercício da aula 25

---
+++ quinzena7/aula25/integra-basica-api/src/config/index.js
@@ -0,0 +1,3 @@
+export const API_URL_BASE = "https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users"
+
+export const HEADER_AUTHORIZATION = {headers: {Authorization:"davi-garcia-tang" }} 
